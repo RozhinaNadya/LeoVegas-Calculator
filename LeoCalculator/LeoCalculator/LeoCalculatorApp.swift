15107585a521e993f6a0640566891e304bafb7ad
Nadya <zhuk.09@mail.ru>
2023-02-16 12:08:10 +0100

    Create the project

---
+++ LeoCalculator/LeoCalculator/LeoCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeoCalculatorApp.swift
+//  LeoCalculator
+//
+//  Created by admin on 2023-02-16.
+//
+
+import SwiftUI
+
+@main
+struct LeoCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
